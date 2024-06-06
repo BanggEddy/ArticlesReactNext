8d41ca69b79904e211bcf2a0d02e99dad546c91b
David LI <108392457+BanggEddy@users.noreply.gitlab.com>
2024-06-06 15:36:13 +0200

    init début project

---
+++ src/pages/Index.jsx
@@ -0,0 +1,23 @@
+import "./../css/App.css";
+
+function Index() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          test
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Index;
